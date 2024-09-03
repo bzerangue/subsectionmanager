e10711f0324e4087ded2fada436e313bacfc2cea
Brian Zerangue <brian.zerangue@gmail.com>
2024-09-03 10:45:03 -0500

    updated for PHP 8.2+

--- content/content.get.php
+++ content/content.get.php
@@ -1,33 +1,36 @@
 <?php
- 
-	/**
-	 * @package content
-	 */
-	require_once(TOOLKIT . '/class.administrationpage.php');
-	require_once(TOOLKIT . '/class.sectionmanager.php');
-	require_once(TOOLKIT . '/class.entrymanager.php');
-	require_once(EXTENSIONS . '/subsectionmanager/lib/class.subsectionmanager.php');
-	
-	class contentExtensionSubsectionmanagerGet extends AdministrationPage {
 
-		/**
-		 * Used to fetch subsection items via an AJAX request.
-		 */
-		public function __viewIndex() {
-			$subsection = new SubsectionManager();
-			
-			// Set flags
-			$flags = SubsectionManager::GETHTML;
-			if(!isset($_GET['entry'])) {
-				$flags |= SubsectionManager::GETALLITEMS;
-			}
+/**
+ * @package content
+ */
+require_once TOOLKIT . '/class.administrationpage.php';
+require_once TOOLKIT . '/class.sectionmanager.php';
+require_once TOOLKIT . '/class.entrymanager.php';
+require_once EXTENSIONS . '/subsectionmanager/lib/class.subsectionmanager.php';
 
-			// Get items
-			$content = $subsection->generate(intval($_GET['id']), intval($_GET['section']), (intval($_GET['entry']) ? intval($_GET['entry']) : NULL), 0, $flags);
-			echo $content['html'];
-			exit;
-		}
+class contentExtensionSubsectionmanagerGet extends AdministrationPage {
+    /**
+     * Used to fetch subsection items via an AJAX request.
+     */
+    public function viewIndex(): void {
+        $subsection = new SubsectionManager();
 
-	}
- 
-?>
+        // Set flags
+        $flags = SubsectionManager::GETHTML;
+        if (!isset($_GET['entry'])) {
+            $flags |= SubsectionManager::GETALLITEMS;
+        }
+
+        // Get items
+        $content = $subsection->generate(
+            (int) $_GET['id'],
+            (int) $_GET['section'],
+            isset($_GET['entry']) ? (int) $_GET['entry'] : null,
+            0,
+            $flags
+        );
+
+        echo $content['html'];
+        exit;
+    }
+}
